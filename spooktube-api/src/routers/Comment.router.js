9caf34e27a2d316ed17f796c0d97b858719ad8fb
Joseph Mitchell <joejmitchell@hotmail.co.uk>
2024-06-25 21:42:51 +0100

    Back-End: Added CommentRouter /comments/video/:videoid route

---
+++ spooktube-api/src/routers/Comment.router.js
@@ -0,0 +1,16 @@
+import Router from "./Router.js";
+
+export default class CommentRouter extends Router {
+    
+    constructor(controller) {
+        super(controller);
+        
+        this._pathRoot = "/comments";
+    }
+    
+    _initialiseRouter() {
+        this._router.get("/video/:videoid", (req, res) => {
+            this._controller.getVideoComments(req, res);
+        });
+    }
+}
