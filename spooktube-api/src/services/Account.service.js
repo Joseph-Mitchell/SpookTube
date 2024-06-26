e67876148cf447a633331ec404585d377cedc4f8
Joseph Mitchell <joejmitchell@hotmail.co.uk>
2024-06-26 13:29:45 +0100

    Back-End: Added AccountService.createAccount

--- spooktube-api/src/services/Account.service.js
+++ spooktube-api/src/services/Account.service.js
@@ -6,4 +6,8 @@ export default class AccountService extends Service {
     async getAccountByUsernameOrEmail(email, username) {
         return await Account.findOne({ $or: [{ email: email }, { username: username }] });
     }
+    
+    async createAccount(username, email, password) {
+        return await Account.create({ username: username, email: email, password: bcrypt.hashSync(password, process.env.HASH_ROUNDS) });
+    }
 }
