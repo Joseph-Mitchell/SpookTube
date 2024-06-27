c217ac218789b8ead48251c8e8603afc74a8a86b
Joseph Mitchell <joejmitchell@hotmail.co.uk>
2024-06-27 15:49:23 +0100

    Make Comment: Integration Test 10 Complete

--- spooktube-api/test/integration/Comment.integration.test.js
+++ spooktube-api/test/integration/Comment.integration.test.js
@@ -229,5 +229,20 @@ describe("Comment Integration Tests", () => {
             //Assert
             assert.equal(actual.status, 400);
         });
+        
+        it("should respond 400 with timeCode > 90", async () => {
+            //Act
+            const actual = await requester
+                .post("/comments/post")
+                .send({
+                    comment: newComments.highTimeCode.comment,
+                    videoId: newComments.highTimeCode.videoId,
+                    timeCode: newComments.highTimeCode.timeCode
+                })
+                .set("authentication", jwt.sign({ id: newComments.highTimeCode.userId }, process.env.SECRET));
+            
+            //Assert
+            assert.equal(actual.status, 400);
+        });
     });
 });
