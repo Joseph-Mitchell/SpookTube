1882618bd09875ebb0c0aae71e2611a036c5de8e
Joseph Mitchell <joejmitchell@hotmail.co.uk>
2024-06-27 09:56:46 +0100

    Front-End: Added login service

---
+++ spooktube-web/src/services/login.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+export default async function login(identifier, password) {    
+    try {
+        const apiUrl = import.meta.env.VITE_APP_BACKEND_URL;
+        
+        const response = await axios({
+            method: "post",
+            url: apiUrl + "/accounts/login",
+            data: {
+                identifier: identifier,
+                password: password,
+            },
+        });
+
+        return response.data;
+    } catch (e) {
+        if (e.response.data) {
+            if (e.response.data.errors)
+                return { message: e.response.data.errors[0].msg };
+            else
+                return { message: e.response.data.message };
+        }
+        else
+            return { message: "Sorry, something went wrong, please try again later." };
+    }
+}
