bd4b3515160b3687418594c96a3372c347e01c9c
Joseph Mitchell <joejmitchell@hotmail.co.uk>
2024-06-28 14:15:06 +0100

    Front-End: Added loginWithToken service

---
+++ spooktube-web/src/services/loginWithToken.js
@@ -0,0 +1,11 @@
+import basicService from "./basicService.js";
+
+export default async function login(token) {
+    const method = "post";
+    const url = "/accounts/token-login";
+    const headers = {
+        authentication: token
+    };
+    
+    return await basicService(method, url, {}, headers);
+}
