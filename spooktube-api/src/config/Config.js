43fa8f8f43e89aa07471e831b923fb9fd143c2c3
Joseph Mitchell <joejmitchell@hotmail.co.uk>
2024-06-29 13:47:30 +0100

    Back-End: Added ContentManagerService.createVideo

--- spooktube-api/src/config/Config.js
+++ spooktube-api/src/config/Config.js
@@ -4,15 +4,6 @@ import cloudinary from "cloudinary";
 export default class Config {
     
     static #nodeEnv = process.env.NODE_ENV;
-    static #cloudinaryOptions = {
-        use_filename: false,
-        unique_filename: true,
-        overwrite: false
-    }
-    
-    static getCloudinaryOptions() {
-        return Config.#cloudinaryOptions;
-    }
     
     static load() {
         config({
--- spooktube-api/src/services/ContentManager.service.js
+++ spooktube-api/src/services/ContentManager.service.js
@@ -1,5 +1,12 @@
 import cloudinary from "cloudinary";
 
 export default class ContentManagerService {
-    
+    async createVideo(videoFile) {
+        return await cloudinary.uploader.upload(videoFile, {
+            resource_type: "video",
+            use_filename: false,
+            unique_filename: true,
+            overwrite: false
+        });
+    }
 }
