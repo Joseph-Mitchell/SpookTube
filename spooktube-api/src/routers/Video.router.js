cf8d144750e6986d7fdbeb2e06a2c0fc496fc453
Joseph Mitchell <joejmitchell@hotmail.co.uk>
2024-06-29 20:57:13 +0100

    Back-End: Added videos/post route

--- spooktube-api/src/routers/Video.router.js
+++ spooktube-api/src/routers/Video.router.js
@@ -1,3 +1,4 @@
+import AccountMiddleware from "../middleware/Account.middleware.js";
 import Router from "./Router.js";
 
 export default class VideoRouter extends Router {
@@ -11,6 +12,9 @@ export default class VideoRouter extends Router {
     _initialiseRouter() {
         this._router.get("/all/:rangeMin/:rangeMax", (req, res) => {
             this._controller.getAllVideos(req, res);
+        });
+        this._router.get("/post", AccountMiddleware.authenticateToken, (req, res) => {
+            this._controller.uploadVideo(req, res);
         });
     }
 }
