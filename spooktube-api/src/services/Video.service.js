a85d96121103ff1cd86bc31a4a47e5e991c11af5
Joseph Mitchell <joejmitchell@hotmail.co.uk>
2024-06-29 14:04:33 +0100

    Back-End: Added VideoService.createVideo

--- spooktube-api/src/services/Video.service.js
+++ spooktube-api/src/services/Video.service.js
@@ -8,4 +8,8 @@ export default class VideoService {
     async getVideosCount() {
         return await Video.find({}).estimatedDocumentCount();
     }
+    
+    async createVideo(videoId, userId) {
+        return await Video.create({ videoId: videoId, userId: userId });
+    }
 }
