ac348978a7b7c401854e7b927d6e7677f0ed8599
Joseph Mitchell <joejmitchell@hotmail.co.uk>
2024-07-01 11:10:07 +0100

    Back-End: Added VideoService.getUserVideos

--- spooktube-api/src/services/Video.service.js
+++ spooktube-api/src/services/Video.service.js
@@ -1,12 +1,16 @@
 import Video from "../models/Video.model.js";
 
 export default class VideoService {
+    async getVideosCount() {
+        return await Video.find({}).estimatedDocumentCount();
+    } 
+
     async getAllVideos() {
         return await Video.find({}).sort({ uploadDate: -1 });
     }
     
-    async getVideosCount() {
-        return await Video.find({}).estimatedDocumentCount();
+    async getUserVideos(userId) {
+        return await Video.find({ userId: userId }).sort({ uploadDate: -1 });
     }
     
     async createVideo(videoId, userId) {
