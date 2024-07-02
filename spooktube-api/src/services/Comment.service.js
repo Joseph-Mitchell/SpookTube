b78b18fdd4d538bda2d122a81181304a96fc12bd
Joseph Mitchell <joejmitchell@hotmail.co.uk>
2024-07-02 10:03:50 +0100

    Back-End: Added CommentService.getUserComments

--- spooktube-api/src/services/Comment.service.js
+++ spooktube-api/src/services/Comment.service.js
@@ -6,6 +6,10 @@ export default class CommentService {
         return await Comment.find({ videoId: videoId }).sort({ timeCode: 1 }).select("-_id comment videoId userId timeCode").populate("userId", "-_id username icon");
     }
     
+    async getUserComments(userId) {
+        return await Comment.find({ userId: userId }).select("-_id comment videoId userId timeCode").populate("userId", "-_id username icon");
+    }
+    
     async createComment(comment, videoId, userId, timeCode) {
         return (await Comment.create({comment: comment, videoId: videoId, userId: userId, timeCode: timeCode})).populate("userId", "-_id username icon");
     }
