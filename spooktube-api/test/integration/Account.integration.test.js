3416aee89d59eb73cc69adf70eed9005de99819c
Joseph Mitchell <joejmitchell@hotmail.co.uk>
2024-07-03 11:36:15 +0100

    Update Profile: Integration Test 5 Complete

--- spooktube-api/test/integration/Account.integration.test.js
+++ spooktube-api/test/integration/Account.integration.test.js
@@ -289,5 +289,20 @@ describe("Account Integration Tests", () => {
             assert.equal(account.username, existingAccounts[0].username);
             assert.equal(account.icon, "0");
         });
+        
+        it("should respond 400 with empty icon", async () => {
+            //Act
+            const actual = await requester
+                .put("/accounts/profile")
+                .send({ username: newAccounts.emptyUsername.username, icon: "" })
+                .set("authentication", jwt.sign({ id: existingAccounts[0]._id }, process.env.SECRET));
+            
+            //Assert
+            const account = await Account.findById(existingAccounts[0]._id);
+
+            assert.equal(actual.status, 400);
+            assert.equal(account.username, existingAccounts[0].username);
+            assert.equal(account.icon, "0");
+        });
     })
 });
