37bef02e80cf5b105d79b2ba44b875e58d6d41cd
Joseph Mitchell <joejmitchell@hotmail.co.uk>
2024-07-04 01:32:09 +0100

    Paginator: Unit Test 1 Complete

---
+++ spooktube-web/tests/components/common/Paginator.test.jsx
@@ -0,0 +1,31 @@
+import { screen } from "@testing-library/dom";
+import { render } from "@testing-library/react";
+import { afterEach, beforeEach, it, vi } from 'vitest';
+
+import Paginator from "../../../src/components/common/Paginator.jsx";
+
+describe("Paginator", () => {
+    let currentPage;
+    let setCurrentPage;
+    let pages;
+
+    beforeEach(() => {
+        currentPage = 1;
+        setCurrentPage = vi.fn();
+        pages = 3;
+    });
+
+    afterEach(() => {
+        currentPage = undefined;
+        setCurrentPage = undefined;
+        pages = undefined;
+    });
+
+    it("should display previous button as disabled if currentPage is 1", () => {
+        //Act
+        render(<Paginator currentPage={currentPage} setCurrentPage={setCurrentPage} pages={pages} />);
+
+        //Assert
+        expect(screen.getByText("Previous")).toHaveClass("link-secondary");
+    });
+});
